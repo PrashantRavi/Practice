d697b8485b19256e36e2b6340a6dc8bfddcd36be
Ravi Parshant <ravi.parshant@Ravis-MacBook-Air.local>
2016-09-29 17:34:06 +0530

    added ?

---
+++ src/arrays/SumofDigits.java
@@ -0,0 +1,20 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class SumofDigits {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number");
+		int n= sc.nextInt();
+		int sum=0;
+		while(n>0){
+			int rem =n%10;
+			sum=sum+rem;
+			n=n/10;
+		}
+        System.out.println(sum);
+	}
+
+}
--- src/testpackage/test.java
+++ src/testpackage/test.java
@@ -1,24 +1,25 @@
 package testpackage;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
 public class test {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args)  {
+		Random r= new Random();
+		int N=30;
+		for(int i=0;i<N;i++){
+			System.out.println(r.nextInt(1000000)+" "+r.nextInt(1000000)+" "+r.nextInt(1000000)+" "+r.nextInt(1000000));
+			System.out.println(r.nextInt(1000000)+" "+r.nextInt(1000000)+" "+r.nextInt(1000000)+" "+r.nextInt(1000000));
 		
-		System.out.println(find(15));
-		
-	}
-	
-	public static int find(int N){
-		int num=N;
-		 int count =0;
-		while (num >1){
-		num=num/2;
-		count++;
+			
 		}
-		return count-1;
+		
+		
 	}
 
 }
