3d65b0ff86d7a1e26e5f556e9070bc7721861467
Ravi Parshant <prashant.ravi3@gmail.com>
2017-01-05 20:57:08 +0530

    Heightest Number with given Number

--- src/testpackage/BlueOptima_MatrixSum.java
+++ src/testpackage/BlueOptima_MatrixSum.java
@@ -1,33 +1,28 @@
 package testpackage;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public class HelloWorld {
+public class BlueOptima_MatrixSum {
 
-
-public static void main(String[] args) {
-		
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T= sc.nextInt();
 		for(int tc=0;tc<T;tc++){
 			int n=sc.nextInt();
 			int sum=0;
-			
 			for(int i=1;i<=n;i++){
-				int j;
-				for(j=1;j<=i/(n/2);j++){
-					sum=sum+i/j;
+				for(int j=1;j<=n;j++){
+					if(i==j){
+						sum++;
+					}else{
+						sum+=i/j;
+					}
 				}
-				sum=sum+i-j+1;
 			}
-			
 			System.out.println(sum);
 		}
-		
-		
+		sc.close();
+
 	}
 
 }
-
-
